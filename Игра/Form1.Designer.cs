443444f88c4da3d96a47e1f9d743e60762515d87
Vladimir901 <47474425+Vladimir901@users.noreply.github.com>
2020-11-03 16:08:58 +0300

    Создал проект и добавил форму для размещения логотипа

--- Игра/Form1.Designer.cs
+++ Игра/Form1.Designer.cs
@@ -29,12 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1394, 705);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Игра/Form1.cs
+++ Игра/Form1.cs
@@ -12,9 +12,37 @@ namespace Игра
 {
     public partial class Form1 : Form
     {
+        int m = 0;
+        Logo logo = new Logo();
         public Form1()
         {
+
             InitializeComponent();
+            this.Hide();
+            Width = 100;
+            Height = 100;
+            
+            timer1.Interval = 1000;          
+            logo.Show();
+            timer1.Start();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            m++;
+            Logotype();
+        }
+        void Logotype()
+        {
+            if (m == 2)
+            {
+                timer1.Stop();
+                logo.Close();
+                this.WindowState = FormWindowState.Maximized;
+                
+            }
         }
     }
 }
